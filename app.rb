450e529988a401aba10bde9cabe6ae0e76e08c46
Bryan Mytko <bryanmytko@gmail.com>
2014-01-28 13:02:44 -0500

    refactoring stuff

--- app.rb
+++ app.rb
@@ -1,5 +1,10 @@
 class HSRanking
-  @winrate = 0
+
+  STREAK_WINS_START = 3
+  STREAK_RANK_END = 5
+  STREAK_EXTRA_STARS = 1
+  MIN_RANK_LOSE_STARS = 20
+
   @@ranks = [
 		25 => 2,
 		24 => 2,
@@ -27,82 +32,88 @@ class HSRanking
 		2 => 5,
 		1 => 5
   ]
+
   def initialize(winrate)
-	  @winrate = winrate
+		@winrate = winrate
+		@rank = 25
+		@streak = 0
+		@stars = 0
+		@total_games_played = 0
+		@total_games_won = 0
 	end
 
   #hide the output for large data sets
-  def puts(string)
-  end
+  #def puts(string)
+  #end
 
 	def random_game_outcome
 		n = case rand*100
 		when 0...@winrate then 1
-		when @winrate..100 then 0
+		when @winrate..100 then nil
 		end
 		n
 	end
 
-  def init
-		streak = 0
-		stars = 0
-		rank = 25
-    total_games_played = 0
-    total_games_won = 0
-    puts "\n"
-    while rank > 0
-      outcome = random_game_outcome
-      total_games_played += 1 
-      if(outcome == 1)
-        total_games_won += 1 
-        puts "Won a game!"
-        stars += 1
-        streak += 1
-        if((streak >= 3) && (rank > 5))
-          stars += 1
-        end
-        if(stars >= @@ranks[0][rank])
-          stars = stars - @@ranks[0][rank]
-          rank -= 1
-        end
-        if(rank > 0)
-					puts "Currently Rank: " + rank.to_s
-					puts "(" + stars.to_s + "/" + @@ranks[0][rank].to_s + ")"
-        end
+  def game_won
+		@total_games_won += 1 
+		@stars += 1
+		@streak += 1
+    #bonus stars for winning streak
+		if((@streak >= STREAK_WINS_START ) && (@rank > STREAK_RANK_END))
+			@stars += 1
+		end
+    #rank up
+		if(@stars >= @@ranks[0][@rank])
+			@stars = @stars - @@ranks[0][@rank]
+			@rank -= 1
+		end
+  end
+
+  def game_lost
+		@streak = 0
+    #we don't always lose stars
+		if(@rank < MIN_RANK_LOSE_STARS)
+			if(@stars > 0)
+				@stars -= 1
+			end
+      #rank down
+			if(@stars == 0)
+				@rank += 1
+				@stars = @@ranks[0][@rank]
+			end
+		end
+  end
+
+  def display_output
+    puts "It took you " + @total_games_played.to_s + " games to reach Legend rank."
+    losses = @total_games_played - @total_games_won
+    puts "Win / Loss ratio: " + @total_games_won.to_s + "/" + losses.to_s + "\n"
+  end
+
+  def start_playing
+    while @rank > 0
+      @total_games_played += 1 
+      if(random_game_outcome)
+        game_won
       else
-        puts "Sorry, you lost a game"
-        streak = 0
-        if(rank < 20)
-          if(stars > 0)
-            stars -= 1
-          end
-          if(stars == 0)
-            puts "going down a rank..."
-            rank += 1
-            stars = @@ranks[0][rank]
-            puts "You've been demoted to rank " + rank.to_s
-          end
-        end
+        game_lost
       end 
     end
-    puts "-----------------------------"
-    puts "Congrats, you achieved Legend!"
-    puts "It took you " + total_games_played.to_s + " games."
-    losses = total_games_played - total_games_won
-    puts "Win / Loss ratio: " + total_games_won.to_s + "/" + losses.to_s
-    total_games_played
+    #display_output
+    @total_games_played
   end
 end
 
-puts "Winrate: "
+#get user input; winrate and how many legendary trips
+puts "Winrate (%)?"
 winrate = gets.to_i
-puts "Sample Size: "
+puts "Sample Size? (How many Legendary runs?) "
 sample = gets.to_i
 
-g = HSRanking.new(winrate)
 num = 0
-games = sample.times.collect { g.init }
-games.each { |a| num += a }
-
+sample.times do
+  g = HSRanking.new(winrate)
+  num += g.start_playing
+end
 
 puts "Average required wins at " + winrate.to_s + "% win rate: " + (num/sample).to_s + " games."
